e2387b56f0729f2651b26b9cd71a98dc0bae94e7
Kaushal Patel <55987371+mrkaushal@users.noreply.github.com>
2023-07-14 06:14:06 +0000

    Added Download Resume Button

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,6 +1,10 @@
 const ResumePage = () => {
+  const fileURL = 'https://drive.google.com/uc?export=download&id=1-TjbOQn7hg4tSmKE0JMtHqfZGXm8UACu';
     return (
       <>
+        <a href={fileURL} rel="noreferrer" style={{display: 'inline-block', marginBottom: '10px', padding: '10px', color: 'white', backgroundColor: '#007BFF', textDecoration: 'none', textAlign: 'left'}}>
+        Download Resume
+        </a>
         <iframe width="100%" height="100%" src="https://drive.google.com/file/d/1-TjbOQn7hg4tSmKE0JMtHqfZGXm8UACu/preview" allow="autoplay"></iframe>
       </>
     );
